c3560b8d91a0345a95d61bde99f3dcb4c46ef9f1
mazhariqbasiddiquee <mazhariqbal963169@gmail.com>
2025-09-15 19:18:46 +0530

    updated the session

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app=express();
 const searchRouter=require('./route/query.js');
 const cors = require('cors');
-const { sessionMiddleware } = require('./middleware/user.js');
+const { logSessionDetails} = require('./middleware/user.js');
 
 
 
@@ -13,7 +13,7 @@ app.use(cors({
   origin: [process.env.Allowed_Hosts, 'http://localhost:5173'], 
   credentials: true                 
 }))
-app.use(sessionMiddleware)
+app.use(logSessionDetails)
 
 
 app.use('/search',searchRouter);
